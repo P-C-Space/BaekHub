4bd5fd92acfb239e3819f0f7e3add04641c27717
P-coding <39722575+P-C-Space@users.noreply.github.com>
2025-06-10 22:02:54 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 308 ms, Memory: 17972 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s  =  new Scanner(System.in);
+		int size = s.nextInt();
+		for(int i =1;i<=size;i++) {
+			for(int j = 1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		s.close();
+	}
+
+}
